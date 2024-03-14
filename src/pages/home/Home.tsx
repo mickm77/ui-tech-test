4cd4cde661815397d075378ff659b11f0d3333ec
mick <mick.mcmahon@gmail.com>
2024-03-14 19:33:32 +0000

    Adds Home page and sets up react query

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchPeople } from "../../api/person";
+import Table from "../../components/table/Table";
+
+const Home = () => {
+  const { data, isLoading, isFetching } = useQuery({
+    queryKey: ["people"],
+    queryFn: fetchPeople,
+  });
+  return <Table data={data} isLoading={isLoading} isFetching={isFetching} />;
+};
+
+export default Home;
---
+++ src/resources/tableDef.tsx
@@ -0,0 +1,56 @@
+export type DefaultTableDef = {
+  type: string;
+  label: string;
+  field: string;
+  sortable: boolean;
+  filterable: boolean;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  cellRenderer?: (value: any) => JSX.Element | string;
+};
+
+export const tableDef = {
+  name: {
+    type: "string",
+    label: "Name",
+    field: "name",
+    sortable: true,
+    filterable: true,
+  },
+  dateOfBirth: {
+    type: "date",
+    label: "Date of Birth",
+    field: "DateOfBirth",
+    sortable: true,
+    filterable: true,
+    cellRenderer: (value: string) => {
+      return new Date(value).toLocaleDateString();
+    },
+  },
+  startDate: {
+    type: "date",
+    label: "Start Date",
+    field: "StartDate",
+    sortable: true,
+    filterable: true,
+    cellRenderer: (value: string) => {
+      return new Date(value).toLocaleDateString();
+    },
+  },
+  active: {
+    type: "boolean",
+    label: "Active",
+    field: "active",
+    sortable: true,
+    filterable: true,
+    cellRenderer: (value: boolean) => {
+      return value ? (
+        <span className={"css.active"}>Active</span>
+      ) : (
+        <span className={"css.inactive"}>Inactive</span>
+      );
+    },
+  },
+};
+
+export type TableDef = typeof tableDef;
